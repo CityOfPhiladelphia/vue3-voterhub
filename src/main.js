458ef20c27d8201ea7ec1a62f25dd75c767ebf77
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-07-17 15:28:45 -0400

    imports phila-ui scss manually instead of importing all.scss file

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import router from './router';
 
 import isMac from './util/is-mac';
 
-import 'vue-good-table-next/dist/vue-good-table-next.css'
-import "bulma";
-import "@fortawesome/fontawesome-pro/css/fontawesome.min.css";
-import "@fortawesome/fontawesome-pro/css/solid.min.css";
-import "@phila/phila-ui-core/dist/styles/scss/all.scss";
+// import 'vue-good-table-next/dist/vue-good-table-next.css'
+// import "bulma";
+// import "../node_modules/bulma/bulma.sass";
+// import "@fortawesome/fontawesome-pro/css/fontawesome.min.css";
+// import "@fortawesome/fontawesome-pro/css/solid.min.css";
+// import "@phila/phila-ui-core/dist/styles/scss/all.scss";
+import './assets/main.scss'
 import './assets/style.scss'
 if (isMac()) {
   import('./assets/mac-style.scss')
