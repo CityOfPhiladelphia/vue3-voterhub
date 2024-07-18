d96f04d70e956a683e815939062bfc17d6dbecf9
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-07-18 14:44:35 -0400

    deletes commented out code

--- src/composables/useRouting.js
+++ src/composables/useRouting.js
@@ -1,11 +1,9 @@
 import { useMainStore } from '@/stores/MainStore';
-import { useParcelsStore } from '@/stores/ParcelsStore';
 
 export default function useRouting() {
   const routeApp = (router) => {
     console.log('routeApp')
     const MainStore = useMainStore();
-    const ParcelsStore = useParcelsStore();
     if (MainStore.currentAddress && MainStore.currentTopic == 'nearby') {
       if (import.meta.env.VITE_DEBUG) console.log('routeApp routing to address-topic-and-data');
       router.push({ name: 'address-topic-and-data', params: { address: MainStore.currentAddress, topic: "nearby", data: MainStore.currentNearbyDataType || '311' } });
@@ -15,10 +13,6 @@ export default function useRouting() {
     } else if (MainStore.currentAddress) {
       if (import.meta.env.VITE_DEBUG) console.log('routeApp routing to address because MainStore has address');
       router.push({ name: 'address', params: { address: MainStore.currentAddress } });
-    // this was from a convoluted plan to use pwd parcels here - not necessary
-      // } else if (MainStore.lastClickCoords == 'mapClick' && ParcelsStore.pwd.features && ParcelsStore.pwd.features.length > 0) {
-    //   if (import.meta.env.VITE_DEBUG) console.log('routeApp routing to address-and-topic because ParcelsStore has pwd features');
-    //   router.push({ name: 'address-and-topic', params: { address: ParcelsStore.pwd.features[0].properties.ADDRESS, topic: MainStore.currentTopic } })
     } else {
       router.push({ name: 'not-found' });
     }
